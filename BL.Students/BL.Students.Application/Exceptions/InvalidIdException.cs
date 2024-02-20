b67c7f6bd77f8924db1b4f11035fb04491bf2298
Federico Dellasoppa <fdellasoppa@gmail.com>
2024-02-20 15:19:00 -0300

    Add service validations

---
+++ BL.Students/BL.Students.Application/Exceptions/InvalidIdException.cs
@@ -0,0 +1,8 @@
+﻿namespace BL.Students.Application.Exceptions;
+
+public class InvalidIdException : Exception
+{
+    public InvalidIdException() : base("Id is not valid")
+    {
+    }
+}
--- BL.Students/BL.Students.Application/Students/StudentService.cs
+++ BL.Students/BL.Students.Application/Students/StudentService.cs
@@ -1,4 +1,5 @@
-﻿using BL.Students.Domain.Students;
+﻿using BL.Students.Application.Exceptions;
+using BL.Students.Domain.Students;
 
 namespace BL.Students.Application.Students;
 
@@ -13,34 +14,63 @@ public class StudentService : IStudentService
 
     public Task CreateAsync(string name, string address, string ssn, CancellationToken cancel)
     {
+        var social = SocialSecurityNumber.Create(ssn);
+        if (social is null)
+            throw new ApplicationException("Ivalid social security number");
+
         var student = new Student(
             new StudentId(new Guid()),
             name,
             address,
-            SocialSecurityNumber.Create(ssn));
+            social);
 
         return _studentRepository.AddAsync(student, cancel);
     }
 
     public Task<Student?> GetAsync(string id, CancellationToken cancel)
     {
-        return _studentRepository.GetAsync(new Guid(id), cancel);
+        if (string.IsNullOrEmpty(id))
+            throw new ArgumentNullException(nameof(id));
+
+        bool valid = Guid.TryParse(id, out Guid guid);
+        if (!valid)
+            throw new InvalidIdException();
+
+        return _studentRepository.GetAsync(guid, cancel);
     }
 
     public async Task<Task> UpdateAsync(string id, string name, string address, string ssn, CancellationToken cancel)
     {
-        var student = await _studentRepository.GetAsync(new Guid(id), cancel);
+        if (string.IsNullOrEmpty(id))
+            throw new ArgumentNullException(nameof(id));
+
+        bool valid = Guid.TryParse(id, out Guid guid);
+        if (!valid)
+            throw new InvalidIdException();
+
+        var student = await _studentRepository.GetAsync(guid, cancel);
 
         if (student is null)
             throw new ApplicationException("Student not found!");
 
+        var social = SocialSecurityNumber.Create(ssn);
+        if (social is null)
+            throw new ApplicationException("Ivalid social security number");
+
         return _studentRepository.UpdateAsync(new Guid(id),
-            new Student(student.Id, name, address, SocialSecurityNumber.Create(ssn)),
+            new Student(student.Id, name, address, social),
             cancel);
     }
 
     public Task DeleteAsync(string id, CancellationToken cancel)
     {
-        return _studentRepository.DeleteAsync(new Guid(id), cancel);
+        if (string.IsNullOrEmpty(id))
+            throw new ArgumentNullException(nameof(id));
+
+        bool valid = Guid.TryParse(id, out Guid guid);
+        if (!valid)
+            throw new InvalidIdException();
+
+        return _studentRepository.DeleteAsync(guid, cancel);
     }
 }
